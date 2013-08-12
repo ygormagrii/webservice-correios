855ec66e747f80530a925c4c5946ceadaa0afade
math <math@saojudas.br>
2013-08-11 23:45:01 -0300

    link in cURL working

--- class/correios.class.php
+++ class/correios.class.php
@@ -24,8 +24,7 @@ class Correios {
 	public function calc() {
 
 		$cURL =/* curl_init(*/sprintf(
-				'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=%s&sDsSenha=%s&nCdServico=%s&sCepOrigem=%s&sCepDestino=%s&nVlPeso=%s&nCdFormato=%s&nVlComprimento=%s& 
-				nVlAltura=%s&nVlLargura=%s&nVlDiametro=%s&sCdMaoPropria=%s&nVlValorDeclarado=%s&sCdAvisoRecebimento=%s&StrRetorno=%s',
+				'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=%s&sDsSenha=%s&nCdServico=%s&sCepOrigem=%s&sCepDestino=%s&nVlPeso=%s&nCdFormato=%s&nVlComprimento=%s&nVlAltura=%s&nVlLargura=%s&nVlDiametro=%s&sCdMaoPropria=%s&nVlValorDeclarado=%s&sCdAvisoRecebimento=%s&StrRetorno=%s',
 				$this->empresa,
 				$this->senha,
 				$this->servico,
@@ -44,6 +43,8 @@ class Correios {
 		/*)*/);
 		
 				var_dump($cURL);
+				echo "<p>";
+				echo "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=09146920&sDsSenha=123456&sCepOrigem=70002900&sCepDestino=71939360&nVlPeso=1&nCdFormato=1&nVlComprimento=30&nVlAltura=30&nVlLargura=30&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=40010&nVlDiametro=0&StrRetorno=xml";
 //  			curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
  		//	$string	= curl_exec($cURL);
  		//var_dump($string);
@@ -51,4 +52,13 @@ class Correios {
 // 			$xml = simplexml_load_string($string);
 // 			var_dump($xml);
 	}
+
+public $error;
+
+public function error() {
+if (is_null($this->error))
+	return false;
+	else
+	return $this->error;
 }
+} // endclass
